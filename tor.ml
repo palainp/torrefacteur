74da57d73c34d131ea05541e273c4fabf0806cca
palainp <pierre.alain@tuta.io>
2022-06-28 11:26:53 +0200

    start to decode 1st router reply

--- tor.ml
+++ tor.ml
@@ -16,6 +16,7 @@
 
 open Lwt.Infix
 open Circuits
+open Tor_constants
 
 (*
    The following should be compatible with:
@@ -98,6 +99,87 @@ module Make (Rand: Mirage_random.S) (Stack: Tcpip.Stack.V4) (KV: Mirage_kv.RO) (
         (* TODO: check for the sha256 against the result in last_list_info *)
         Lwt.return nodes
 
+
+
+    let escape_data buf = String.escaped (Cstruct.to_string buf)
+
+    let write tls buf =
+        TLS.write tls buf >>= function
+        | Ok () -> Log.info(fun f -> f "send %s" (escape_data buf)); Lwt.return (Ok())
+        | Error e -> Log.info(fun f -> f "send err: %a" TLS.pp_write_error e); Lwt.return (Error e)
+
+    let read tls =
+        TLS.read tls >>= function
+        | Ok (`Data buf) -> Log.info(fun f -> f "recv %s" (escape_data buf)); Lwt.return (Ok buf)
+        | Ok `Eof -> Log.info(fun f -> f "recv eof"); Lwt.return (Ok Cstruct.empty)
+        | Error e -> Log.info(fun f -> f "recv err: %a" TLS.pp_error e); Lwt.return (Error e)
+
+    let uint8_to_cs i =
+        let cs = Cstruct.create 1 in
+        Cstruct.set_uint8 cs 0 i;
+        cs
+
+    let uint16_to_cs i =
+        let cs = Cstruct.create 2 in
+        Cstruct.BE.set_uint16 cs 0 i;
+        cs
+
+    let uint32_to_cs i =
+        let cs = Cstruct.create 4 in
+        Cstruct.BE.set_uint32 cs 0 i;
+        cs
+
+    let version circID =
+        let v = Cstruct.concat [
+            uint16_to_cs 3;                 (* claims to be a version 3 client only *)
+        ] in
+        let len = Cstruct.length v in
+        let payload = Cstruct.concat [
+            circID ;
+            uint8_to_cs (tor_command_to_uint8 VERSIONS) ;
+            uint16_to_cs len ;
+            v ;
+        ] in
+        payload
+
+    (* 5.1 CREATE and CREATED cells *)
+    let create2 cirdID hdata =
+        let len = Cstruct.length hdata in
+        let payload = Cstruct.concat [
+            cirdID ;
+            uint8_to_cs (tor_command_to_uint8 CREATE2) ;
+            uint16_to_cs 0 ;   (* HTYPE *)
+            uint16_to_cs len ; (* HLEN *)
+            hdata              (* HDATA *)
+        ] in
+        payload
+
+    let created2 cirdID hdata =
+        let len = Cstruct.length hdata in
+        Cstruct.concat [
+            cirdID ;
+            uint8_to_cs (tor_command_to_uint8 CREATED2) ;
+            uint16_to_cs len ; (* HLEN *)
+            hdata              (* HDATA *)
+        ]
+
+    (* 5.1 CREATE and CREATED cells *)
+    let extend2 cirdID lspec hdata =
+        let rec lspec_to_cstruct lspec acc =
+            match lspec with
+            | [] -> acc
+            | lspec::t ->
+                Cstruct.concat [lspec ; lspec_to_cstruct t acc]
+        in
+        let _len = Cstruct.length hdata in
+        Cstruct.concat [
+            cirdID ;
+            uint8_to_cs (tor_command_to_uint8 CREATED2) ;
+            uint8_to_cs (List.length lspec) ;
+            lspec_to_cstruct lspec hdata
+        ]
+
+
 (*
 5.3. Creating circuits
 
@@ -124,13 +206,117 @@ module Make (Rand: Mirage_random.S) (Stack: Tcpip.Stack.V4) (KV: Mirage_kv.RO) (
         let rec add_relays n circuit =
             match n with
             | 0 -> circuit
-            | x -> 
+            | x ->
                 let rnd_relay = Random.int (List.length relay) in
                 (* TODO: ensure that no router appears in the path twice *)
                 let circuit = Circuits.add_relay circuit (List.nth relay rnd_relay) in
                 add_relays (x-1) circuit
         in
         Lwt.return (add_relays (n-1) circuit)
+
+
+    let parse_pack payload =
+      let rec consume_message payload acc =
+          let len_payload = Cstruct.length payload in
+          if len_payload < 3 then Lwt.return acc
+          else begin
+            let _id = Cstruct.sub payload 0 2 in
+            let typ = tor_command_of_uint8 (Cstruct.get_uint8 payload 2) in
+            let payload = Cstruct.shift payload 3 in
+            match typ with
+
+            | VERSIONS ->
+                let len = Cstruct.BE.get_uint16 payload 0 in
+                let _versions = Cstruct.sub payload 2 len in
+                consume_message (Cstruct.shift payload (2+len)) (List.cons (VERSIONS, Cstruct.sub payload 0 (2+len)) acc)
+
+            | CERTS ->
+                let len = Cstruct.BE.get_uint16 payload 0 in
+                let ncerts = Cstruct.get_uint8 payload 2 in
+                let rec parse_certs n payload consumed_size =
+                    match n with
+                    | 0 ->
+                        consumed_size
+                    | n ->
+                        let _cert_type = Cstruct.get_uint8 payload 0 in
+                        let clen = Cstruct.BE.get_uint16 payload 1 in
+                        let _cert = Cstruct.sub payload 3 clen in
+                        parse_certs (n-1) (Cstruct.shift payload (3+clen)) (consumed_size+3+clen)
+                in
+                let _consumed_size = parse_certs ncerts (Cstruct.shift payload 3) 0 in
+                (* assert _consumed_size == len *)
+                consume_message (Cstruct.shift payload (2+len)) (List.cons (CERTS, Cstruct.sub payload 0 (2+len)) acc)
+
+            | AUTH_CHALLENGE ->
+                let len = Cstruct.BE.get_uint16 payload 0 in
+                let _challenge = Cstruct.sub payload 2 32 in
+                let n_methods = Cstruct.BE.get_uint16 payload 34 in
+                let rec parse_methods n payload consumed_size =
+                    match n with
+                    | 0 ->
+                        consumed_size
+                    | n ->
+                        let _method = Cstruct.BE.get_uint16 payload 0 in
+                        parse_methods (n-1) (Cstruct.shift payload 2) (consumed_size+2)
+                in
+                let _consumed_size = parse_methods n_methods (Cstruct.shift payload 8) 0 in
+                (* assert _consumed_size == len *)
+                consume_message (Cstruct.shift payload (2+len)) (List.cons (AUTH_CHALLENGE, Cstruct.sub payload 0 (2+len)) acc)
+
+            | NETINFO ->
+                let ip_len_of_cstruct v =
+                    match v with
+                        | 4 -> 4
+                        | 6 -> 16
+                        | _ -> Log.err (fun m -> m "Unexpected value when reading the IP addr size"); 0
+                in
+                let _timestamp = Cstruct.BE.get_uint32 payload 0 in
+                let _other_atype = Cstruct.get_uint8 payload 4 in
+                let other_alen = ip_len_of_cstruct (Cstruct.get_uint8 payload 5) in
+                let _other_aval = Cstruct.sub payload 6 other_alen in
+
+                let rec parse_my_addr n payload consumed_size =
+                    match n with
+                    | 0 ->
+                        consumed_size
+                    | n ->
+                        let _my_atype = Cstruct.get_uint8 payload 0 in
+                        let my_alen = ip_len_of_cstruct (Cstruct.get_uint8 payload 1) in
+                        let _my_aval = Cstruct.sub payload (2+other_alen) my_alen in
+                        parse_my_addr (n-1) (Cstruct.shift payload (2+my_alen)) (consumed_size+2+my_alen)
+                in
+                let n_my_addr = Cstruct.get_uint8 payload (6+other_alen) in
+                let consumed_size = parse_my_addr n_my_addr (Cstruct.shift payload (6+other_alen+1)) 0 in
+
+                consume_message (Cstruct.shift payload (6+other_alen+1+consumed_size)) (List.cons (AUTH_CHALLENGE, Cstruct.sub payload 0 (6+other_alen+1+consumed_size)) acc)
+
+            | _ ->
+                Cstruct.hexdump payload ;
+                Lwt.return acc
+          end
+      in
+      consume_message payload []
+
+
+    (* 4. Negotiating and initializing connections
+       When the in-protocol handshake is used, the initiator sends a
+   VERSIONS cell to indicate that it will not be renegotiating.  The
+   responder sends a VERSIONS cell, a CERTS cell (4.2 below) to give the
+   initiator the certificates it needs to learn the responder's
+   identity, an AUTH_CHALLENGE cell (4.3) that the initiator must include
+   as part of its answer if it chooses to authenticate, and a NETINFO
+   cell (4.5). *)
+    let negotiate_version tls circID =
+        write tls (version circID) >>= fun _ ->
+        read tls >|= function
+        | Error e ->
+            Log.err (fun m -> m "error %a while receiving packets" TLS.pp_error e ) ;
+            Lwt.return_unit
+        | Ok data ->
+            parse_pack data >>= fun packets ->
+            Log.info (fun m -> m "got %d packets" (List.length packets));
+            Lwt.return_unit
+
 
 (*
       3. If not already connected to the first router in the chain,
@@ -147,85 +333,6 @@ module Make (Rand: Mirage_random.S) (Stack: Tcpip.Stack.V4) (KV: Mirage_kv.RO) (
       6. For each subsequent onion router R (R_2 through R_N), extend
          the circuit to R.
 *)
-    let escape_data buf = String.escaped (Cstruct.to_string buf)
-
-    let write tls buf =
-        TLS.write tls buf >>= function
-        | Ok () -> Log.info(fun f -> f "send %s" (escape_data buf)); Lwt.return (Ok())
-        | Error e -> Log.info(fun f -> f "send err: %a" TLS.pp_write_error e); Lwt.return (Error e)
-
-    let read tls =
-        TLS.read tls >>= function
-        | Ok (`Data buf) -> Log.info(fun f -> f "recv %s" (escape_data buf)); Lwt.return (Ok())
-        | Ok `Eof -> Log.info(fun f -> f "recv eof"); Lwt.return (Ok())
-        | Error e -> Log.info(fun f -> f "recv err: %a" TLS.pp_error e); Lwt.return (Error e)
-
-    let uint8_to_cs i =
-        let cs = Cstruct.create 1 in
-        Cstruct.set_uint8 cs 0 i;
-        cs
-
-    let uint16_to_cs i =
-        let cs = Cstruct.create 2 in
-        Cstruct.BE.set_uint16 cs 0 i;
-        cs
-
-    let uint32_to_cs i =
-        let cs = Cstruct.create 4 in
-        Cstruct.BE.set_uint32 cs 0 i;
-        cs
-
-    (* 4.1 VERSION cells *)
-    let version circID =
-        let v = Cstruct.concat [
-            uint8_to_cs 1;
-        ] in
-        let len = Cstruct.length v in
-        let payload = Cstruct.concat [
-            circID ;
-            uint8_to_cs (Tor_constants.tor_command_to_uint8 VERSIONS) ;
-            uint16_to_cs len ;
-            v ;
-        ] in
-        payload
-
-    (* 5.1 CREATE and CREATED cells *)
-    let create2 cirdID hdata =
-        let len = Cstruct.length hdata in
-        let payload = Cstruct.concat [
-            cirdID ;
-            uint8_to_cs (Tor_constants.tor_command_to_uint8 CREATE2) ;
-            uint16_to_cs 0 ;   (* HTYPE *)
-            uint16_to_cs len ; (* HLEN *)
-            hdata              (* HDATA *)
-        ] in
-        payload
-
-    let created2 cirdID hdata =
-        let len = Cstruct.length hdata in
-        Cstruct.concat [
-            cirdID ;
-            uint8_to_cs (Tor_constants.tor_command_to_uint8 CREATED2) ;
-            uint16_to_cs len ; (* HLEN *)
-            hdata              (* HDATA *)
-        ]
-
-    (* 5.1 CREATE and CREATED cells *)
-    let extend2 cirdID lspec hdata =
-        let rec lspec_to_cstruct lspec acc =
-            match lspec with
-            | [] -> acc
-            | lspec::t ->
-                Cstruct.concat [lspec ; lspec_to_cstruct t acc]
-        in
-        let _len = Cstruct.length hdata in
-        Cstruct.concat [
-            cirdID ;
-            uint8_to_cs (Tor_constants.tor_command_to_uint8 CREATED2) ;
-            uint8_to_cs (List.length lspec) ;
-            lspec_to_cstruct lspec hdata
-        ]
-
     let connect_circuit stack _kv circuit =
         (* TODO: if circuit.relay is empty, only use the exit node... *)
         (* 3. *)
@@ -251,28 +358,8 @@ module Make (Rand: Mirage_random.S) (Stack: Tcpip.Stack.V4) (KV: Mirage_kv.RO) (
         (* 4. *)
                 let circID = uint16_to_cs (1024) in
                 (* assert circID <> 0 and was never used with the first node *)
-                write tls (version circID) >>= fun _ ->
-                Log.info (fun m -> m "version sended");
+                negotiate_version tls circID >>= fun _ ->
         (* 5. *)
-                read tls >>= function
-                | Error e ->
-                    Log.err (fun m -> m "error %a while receiving CREATED packet from %a:%d"
-                            TLS.pp_error e Ipaddr.V4.pp first_node.ip_addr first_node.port) ;
-                    Lwt.return_unit
-                | Ok _buf ->
         (* 6. *)
-                    Log.info (fun m -> m "read from TLS connection %a:%d"
-                      Ipaddr.V4.pp first_node.ip_addr first_node.port);
-                    write tls (create2 circID (Cstruct.create 0)) >>= fun _ ->
-                    Log.info (fun m -> m "create2 NULL sended");
-                    let rec extend_circuit nodes =
-                        match nodes with
-                        | [] -> (* TODO: extend to exit *) Lwt.return_unit
-                        | _n::t ->
-                            (* extend to the next node *)
-                            write tls (extend2 circID [] (Cstruct.create 0)) >>= fun _ ->
-                            extend_circuit t
-                    in
-                    extend_circuit (List.tl circuit.relay) >>= fun _ ->
-                    Lwt.return_unit
+                Lwt.return_unit
 end
